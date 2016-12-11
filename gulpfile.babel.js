1e633546d049ba97cefb3541041a02a9e2b6f0fb
Takeshi SHINODA <takeshinoda@gmail.com>
2016-12-12 00:41:13 +0900

    fix gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,26 +9,11 @@ import runSequence from 'run-sequence'
 const SOURCES    = ['src/**/*.js']
 const TEST_FILES = ['test/**/*_test.js', '!test/fixtures/*.js']
 
-gulp.task('test', ['compile'], () => {
+gulp.task('test', () => {
   return gulp.src(TEST_FILES)
              //.pipe(mocha({ timeout: 25000, reporter: 'nyan' }))
              .pipe(mocha({ timeout: 25000, reporter: 'dot' }))
 })
-
-gulp.task('compile', () => {
-  return gulp.src(SOURCES)
-             .pipe(babel())
-             .pipe(gulp.dest('lib'))
-})
-
-gulp.task('watch', (done) => {
-  gulp.watch(SOURCES.concat(TEST_FILES), ['default'])
-      .on('end', done)
-})
-
-gulp.task('publish', ['compile'], (done) => {
-  child_process.spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
-});
 
 gulp.task('gyp_configure', (done) => {
   child_process.spawn('node-gyp', ['configure'], { stdio: 'inherit' }).on('close', done);
